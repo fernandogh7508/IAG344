1c3ba23e87ca65a9125d3988a273f392cd7c3089
Profe Luis y Julian <alexduqueg123@gmail.com>
2026-01-15 11:36:05 -0500

    Entrenamiento md

--- Mision1/proyecto1/entrenamiento.py
+++ Mision1/proyecto1/entrenamiento.py
@@ -1,2 +1,12 @@
+# librerías
 import re
+"""
+Espresiones regulares en Python
+Problemas Reales 
+"""
+#Codigo
 print("Librería cargada correctamente")
+# Ejemplo1
+texto="Mi Número es 12345"
+resultado=re.search(r"\d+",texto)
+print(resultado.group())   
