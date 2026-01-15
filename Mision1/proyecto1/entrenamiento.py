2713667d3407a7ce032f6382d3b8e8acb04dab4a
Profe Luis y Julian <alexduqueg123@gmail.com>
2026-01-15 13:24:37 -0500

    Back ok

--- Mision1/proyecto1/entrenamiento.py
+++ Mision1/proyecto1/entrenamiento.py
@@ -9,4 +9,8 @@ print("Librería cargada correctamente")
 # Ejemplo1
 texto="Mi Número es 12345"
 resultado=re.search(r"\d+",texto)
-print(resultado.group())   
+print(f"{texto} Resultado {resultado.group()}")   
+texto="Mi Número es 12345-985"
+resultado=re.search(r"\d+",texto)
+print(f"{texto} Resultado {resultado.group()}")   
+  
