63539db1d3f8f609bbb7c8ed3500acb14903617d
Profe Luis y Julian <alexduqueg123@gmail.com>
2026-01-29 11:09:29 -0500

    pwv1

---
+++ Mision3/chatbotpw/chatbot/model.py
@@ -0,0 +1,28 @@
+import os
+from groq import Groq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def generate_response(user_text,system_promt=None):
+    api_key = os.getenv("GROQ_API_KEY")
+    if not api_key:
+        return "❌  Error  de  configuración  de  la api "
+    client = Groq(api_key=api_key)
+    messages =[]
+    if system_promt:
+        messages.append({
+            "role": "system",
+            "content": system_promt
+            })
+    messages.append({
+        "role":"user",
+        "content":user_text
+        })
+
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=messages,
+        temperature=0.3
+        )
+    return response.choices[0].message.cohtent
