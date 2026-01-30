f8b9366fd4b7ad48982cb850acf1e666b1b3a670
Profe Luis y Julian <alexduqueg123@gmail.com>
2026-01-30 13:44:13 -0500

    despliegue PAW

--- Mision3/chatbotpw/chatbot/model.py
+++ Mision3/chatbotpw/chatbot/model.py
@@ -2,27 +2,35 @@ import os
 from groq import Groq
 from dotenv import load_dotenv
 
+# Cargar .env SOLO en local (no rompe en producción)
 load_dotenv()
 
-def generate_response(user_text,system_promt=None):
+
+def generate_response(user_text, system_prompt=None):
     api_key = os.getenv("GROQ_API_KEY")
+
     if not api_key:
-        return "❌  Error  de  configuración  de  la api "
+        return "❌ Error de configuración: falta la API Key de Groq."
+
     client = Groq(api_key=api_key)
-    messages =[]
-    if system_promt:
+
+    messages = []
+
+    if system_prompt:
         messages.append({
             "role": "system",
-            "content": system_promt
-            })
-    messages.append({
-        "role":"user",
-        "content":user_text
+            "content": system_prompt
         })
+
+    messages.append({
+        "role": "user",
+        "content": user_text
+    })
 
     response = client.chat.completions.create(
         model="llama-3.3-70b-versatile",
         messages=messages,
         temperature=0.3
-        )
-    return response.choices[0].message.cohtent
+    )
+
+    return response.choices[0].message.content
